224c517c987b32cdd88b64010fd882dc491ed6c1
Ruoxi Yang <ruoxiy@andrew.cmu.edu>
2023-12-02 09:20:00 -0500

    commented bug free except modifier

--- emptyPT/ContentView.swift
+++ emptyPT/ContentView.swift
@@ -1,53 +1,87 @@
 import SwiftUI
 import PerspectiveTransform
+import UIKit
+import Foundation
+import QuartzCore
+import CoreGraphics
 
 struct ContentView: View {
-    @State private var cornerPoints = [
-        CGPoint(x: 100, y: 100),    // Top left
-        CGPoint(x: 300, y: 100),    // Top right
-        CGPoint(x: 100, y: 300),    // Bottom left
-        CGPoint(x: 300, y: 300)     // Bottom right
-    ]
+//    @State private var cornerPoints = [
+//        CGPoint(x: 0, y: 0),    // Top left
+//        CGPoint(x: 400, y: 0),    // Top right
+//        CGPoint(x: 0, y: 400),    // Bottom left
+//        CGPoint(x: 400, y: 400)     // Bottom right
+//    ]
+    @State private var topLeft = CGPoint(x: 0, y: 0) // 子视图/正方形 的左上角
+    @State private var topRight = CGPoint(x: 400, y: 0)
+    @State private var bottomLeft = CGPoint(x: 0, y: 400)
+    @State private var bottomRight = CGPoint(x: 400, y: 400)
 
     var body: some View {
         GeometryReader { geometry in
             let center = CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2)
-            let initialOffset = CGPoint(x: center.x - 200, y: center.y - 200)
+            let initialOffset = CGPoint(x: 0, y: 230) // 初始左上
 
             ZStack {
-                Image("baseImage")
-                    .resizable()
-                    .frame(width: 400, height: 400)
-                    .position(center)
+//                Image("baseImage")
+//                    .resizable()
+//                    .frame(width: 400, height: 400)
+//                    .position(center)
 
-                OverlayImageView(cornerPoints: $cornerPoints)
+              OverlayImageView(topLeft: $topLeft, topRight: $topRight, bottomLeft: $bottomLeft, bottomRight: $bottomRight)
                     .frame(width: 400, height: 400)
+//                    .offset(x: 0, y: 0)
                     .position(center)
-                    .opacity(0.5)
+//                    .opacity(0.5)
 
-                ForEach(Array(zip(cornerPoints.indices, cornerPoints)), id: \.0) { index, point in
-                    Circle()
-                        .fill(Color.blue)
-                        .frame(width: 30, height: 30)
-                        .position(x: point.x + initialOffset.x, y: point.y + initialOffset.y)
-                        .gesture(DragGesture()
-                            .onChanged({ value in
-                                // Update corner positions
-                                cornerPoints[index] = CGPoint(x: value.location.x - initialOffset.x, y: value.location.y - initialOffset.y)
-                            }))
-                }
+//                ForEach(Array(zip(cornerPoints.indices, cornerPoints)), id: \.0) { index, point in
+//                    Circle()
+//                        .fill(Color.blue)
+//                        .frame(width: 30, height: 30)
+//                        .position(x: point.x + initialOffset.x, y: point.y + initialOffset.y)
+//                        .gesture(DragGesture()
+//                            .onChanged({ value in
+//                                // Update corner positions
+//                                cornerPoints[index] = CGPoint(x: value.location.x - initialOffset.x, y: value.location.y - initialOffset.y)
+//                            }))
+//                }
+                DraggableCornerView(position: $topLeft, initialOffset: initialOffset)
+                DraggableCornerView(position: $topRight, initialOffset: initialOffset)
+                DraggableCornerView(position: $bottomLeft, initialOffset: initialOffset)
+                DraggableCornerView(position: $bottomRight, initialOffset: initialOffset)
             }
         }
         .edgesIgnoringSafeArea(.all)
+        .background(Color.gray)
+    }
+}
+
+struct DraggableCornerView: View {
+    @Binding var position: CGPoint
+    var initialOffset: CGPoint
+
+    var body: some View {
+        Circle()
+            .fill(Color.blue)
+            .frame(width: 30, height: 30)
+            .position(x: position.x + initialOffset.x, y: position.y + initialOffset.y) //正方形在长方形上的位置：0（正方形的右上角相对于正方形的左上角的位置） + offset
+            .gesture(DragGesture()
+                // change 正方形的corner
+                .onChanged({ value in
+                    position = CGPoint(x: value.location.x - initialOffset.x, y: value.location.y - initialOffset.y)
+                }))
     }
 }
 
 struct OverlayImageView: View {
-    @Binding var cornerPoints: [CGPoint]
-
+//    @Binding var cornerPoints: [CGPoint]
+    @Binding var topLeft: CGPoint
+    @Binding var topRight: CGPoint
+    @Binding var bottomLeft: CGPoint
+    @Binding var bottomRight: CGPoint
     var body: some View {
         let startPerspective = Perspective(CGRect(x: 0, y: 0, width: 400, height: 400))
-        let endPerspective = Perspective(cornerPoints)
+        let endPerspective = Perspective(topLeft, topRight, bottomLeft, bottomRight)
 
         return Image("overlayImage")
             .resizable()
@@ -70,3 +104,5 @@ struct PerspectiveModifier: GeometryEffect {
         return ProjectionTransform(affineTransform)
     }
 }
+
+
